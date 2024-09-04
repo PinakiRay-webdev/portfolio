22e64d13249374f3934a252081cb6e610cab3492
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-09-04 12:26:00 +0530

    enhancements

--- frontend/src/Components/Avatar.jsx
+++ frontend/src/Components/Avatar.jsx
@@ -10,7 +10,7 @@ const Avatar = () => {
         gsap.to("#hero-text" , {opacity:1, delay:2})
         tl.from("#avatarImg" , {opacity:0,  delay:2 , top:72})
         tl.from("#tag" , {opacity:0})
-        tl.from("#social" , {opacity:0 , duration:1 , stagger:0.3})
+        tl.from("#social" , {opacity:0 , duration:1 , stagger:0.3 , y:-20})
 
     })
     
@@ -23,7 +23,7 @@ const Avatar = () => {
     </h1>
     <div className='flex gap-6 absolute bottom-10' >
       {socialMedia.map((Element , id)=>(
-        <img id='social' className='w-8'  key={id} src={Element} alt="" />
+        <img id='social' className='w-8 cursor-pointer'  key={id} src={Element} alt="" />
       ))}
     </div>
     </div>
